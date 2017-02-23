a7e47afda15aa0c53622a4d557cec50a3eb4d6a2
java2019 <java_2019@mail.ru>
2017-02-23 13:30:08 +0300

    Parsing and top menu

--- app/src/main/java/com/example/use/monocler/MainActivity.java
+++ app/src/main/java/com/example/use/monocler/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.use.monocler;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +10,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /*Document document = null;
+        try {
+        document = Jsoup.connect("https://monocler.ru/karta-sayta").get();
+
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+
+    Elements elements = document.getElementsByClass("wsp-category-title");
+        for (Element element : elements) {
+        System.out.println(element);
+    }*/
 }
